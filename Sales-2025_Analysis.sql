2ecfb3eab6dcbcec611b878ada3aee59774c5cd8
AmitSakhare <66629728+AmitSakhare@users.noreply.github.com>
2025-09-13 17:03:17 +0530

    commit

--- Sales-2025_Analysis.sql
+++ Sales-2025_Analysis.sql
@@ -19,3 +19,62 @@ Concity VARCHAR(100),
 PtyGSTin VARCHAR(25),	
 Transporter VARCHAR(100)
 );
+
+-- FEATCH TOP 10 RECORDS 
+SELECT * FROM SALES_DB LIMIT 10;
+
+-- FEATCH COUNT THE NUMBER OF ENTRY
+SELECT COUNT(*) FROM SALES_DB;
+
+-- DATA Cleaning for all columns check null values
+SELECT * FROM SALES_dB WHERE
+INVOICE_ID IS NULL OR
+INVOICENO IS NULL OR
+PTYNAME IS NULL OR
+CONNAME IS NULL OR
+CONCITY IS NULL OR
+PTYGSTIN IS NULL OR
+TRANSPORTER IS NULL;
+
+-- How many sales we have?
+SELECT ROUND(SUM(TOTWEIGHT)) FROM SALES_DB; 
+
+-- How many uniuque party's we have ?
+SELECT COUNT(DISTINCT PTYNAME) FROM SALES_DB;
+
+-- Which city has maximum party's ?
+SELECT partycity, COUNT(DISTINCT PTYNAME) AS PARTY_COUNT FROM sales_db 
+GROUP BY partycity ORDER BY PARTY_COUNT DESC LIMIT 1;
+
+-- Which Party has maximum Consignee's ?
+SELECT PTYNAME, COUNT(DISTINCT CONNAME) AS CON_COUNT FROM sales_db 
+GROUP BY PTYNAME ORDER BY CON_COUNT DESC LIMIT 1;
+
+-- Q.1 Write a SQL query to retrieve all columns for sales made on Month of May
+SELECT * FROM SALES_DB WHERE INVOICEDATE BETWEEN '2025-05-01' AND '2025-05-30';
+-- Q.2 Write a SQL query to cou how many sales we have in may month
+SELECT ROUND(SUM(TOTWEIGHT)) FROM SALES_DB WHERE INVOICEDATE BETWEEN '2025-05-01' AND '2025-05-30';
+
+-- Q.3 Write a SQL query to find out how many unique party's we have in may month
+SELECT COUNT(DISTINCT PTYNAME) FROM SALES_DB WHERE INVOICEDATE BETWEEN '2025-05-01' AND '2025-05-30';
+-- Q.4 Which city has maximum party's in may month
+SELECT partycity, COUNT(DISTINCT PTYNAME) AS PARTY_COUNT FROM sales_db 
+WHERE INVOICEDATE BETWEEN '2025-05-01' AND '2025-05-30' GROUP BY partycity ORDER BY PARTY_COUNT DESC LIMIT 1;
+
+-- Q.5 Which Party has maximum Consignee's in may month
+SELECT PTYNAME, COUNT(DISTINCT CONNAME) AS CON_COUNT FROM sales_db 
+WHERE INVOICEDATE BETWEEN '2025-05-01' AND '2025-05-30' GROUP BY PTYNAME ORDER BY CON_COUNT DESC LIMIT 1;
+
+-- Q.6 Write a SQL query to retrieve all columns for sales made on Month of June
+SELECT * FROM SALES_DB WHERE INVOICEDATE BETWEEN '2025-06-01' AND '2025-06-30';
+-- Q.7 Write a SQL query to cou how many sales we have in june month   
+SELECT ROUND(SUM(TOTWEIGHT)) FROM SALES_DB WHERE INVOICEDATE BETWEEN '2025-06-01' AND '2025-06-30';
+
+-- Q.8 Write a SQL query to calculate total sales amount
+SELECT ptyname, ROUND(SUM(TOTWEIGHT * RATE)) FROM SALES_DB
+GROUP BY ptyname ORDER BY ROUND(SUM(TOTWEIGHT * RATE)) DESC;
+
+-- Q.9 Write a SQL query to calculate total sales amount month wise 
+SELECT to_char(INVOICEDATE, 'month') AS SALE_MONTH, ROUND(SUM(TOTWEIGHT * RATE)) AS TOTAL_SALES_AMOUNT
+FROM SALES_DB
+GROUP BY SALE_MONTH ORDER BY SALE_MONTH;
